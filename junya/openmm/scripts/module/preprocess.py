d94dec78595f33a3cabc2eb28afcd9a8acd6e9b5
Razvan Marinescu <mrazvan22@gmail.com>
2023-11-26 14:30:26 -0800

    added junya's folder

---
+++ junya/openmm/scripts/module/preprocess.py
@@ -0,0 +1,86 @@
+import openmm as mm
+import openmm.app as app
+from openmm import unit
+import pdbfixer
+import requests
+from module import function
+
+
+def prepare_protein(pdbid=str):
+    """
+    Preprocesses a protein by downloading the PDB file, fixing missing residues and atoms,
+    adding missing hydrogens, adding solvent, and writing the processed PDB file.
+
+    Args:
+        pdbid (str): The PDB ID of the protein.
+
+    Returns:
+        None
+    """
+    
+    print(f"Preprocess of {pdbid}")
+    # create folder
+    function.create_folder(f"../data/{pdbid}")
+    pdb_path = f"../data/{pdbid}/raw/{pdbid}.pdb"
+    pdb_url = f"https://files.rcsb.org/download/{pdbid}.pdb"
+
+    # download pdb file
+    r = requests.get(pdb_url)
+    r.raise_for_status()
+    with open(pdb_path, "wb") as f:
+        f.write(r.content)
+
+    fixer = pdbfixer.PDBFixer(pdb_path)
+    # remove heterogens other than water
+    fixer.removeHeterogens()
+
+    # find missing residues and atoms
+    fixer.findMissingResidues()
+    fixer.findMissingAtoms()
+    print(f"Missing residues: {fixer.missingResidues}")
+    print(f"Missing terminals: {fixer.missingTerminals}")
+    print(f"Missing atoms: {fixer.missingAtoms}")
+
+    # remove missing residues at the terminal
+    chains = list(fixer.topology.chains())
+    keys = fixer.missingResidues.keys()
+    for key in list(keys):
+        chain = chains[key[0]]
+        # terminal residues
+        if key[1] == 0 or key[1] == len(list(chain.residues())):
+            del fixer.missingResidues[key]
+
+    # check if the terminal residues are removed
+    for key in list(keys):
+        chain = chains[key[0]]
+        assert key[1] != 0 or key[1] != len(list(chain.residues())), "Terminal residues are not removed."
+
+    # find nonstandard residues
+    fixer.findNonstandardResidues()
+    fixer.replaceNonstandardResidues()
+
+    # add missing atoms, residues, and terminals
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms()
+
+    # add missing hydrogens
+    ph = 7.0
+    fixer.addMissingHydrogens(ph)
+
+    print("\nAfter the process")
+    print(f"Missing residues: {fixer.missingResidues}")
+    print(f"Missing terminals: {fixer.missingTerminals}")
+    print(f"Missing atoms: {fixer.missingAtoms}")
+
+    # set the forcefield
+    forcefield = app.ForceField("amber14-all.xml", "amber14/tip3pfb.xml")
+
+    # make modeller
+    # solvent model: tip3p. Default is NaCl
+    modeller = app.Modeller(fixer.topology, fixer.positions)
+    modeller.addSolvent(forcefield, padding=1.0 * unit.nanometers, ionicStrength=0.15 * unit.molar)
+
+    # write the processed pdb file
+    top = modeller.getTopology()
+    pos = modeller.getPositions()
+    app.PDBFile.writeFile(top, pos, open(f'../data/{pdbid}/processed/{pdbid}_processed.pdb', 'w'))
---
+++ junya/openmm/scripts/module/simulation.py
@@ -0,0 +1,148 @@
+from openmm import *
+from openmm.app import *
+from openmm.unit import *
+import numpy as np
+from mdtraj.reporters import HDF5Reporter
+import h5py
+import datetime
+from importlib import reload
+from mdtraj.reporters import HDF5Reporter
+
+def run(pdbid=str, input_pdb_path=str, atomSubset=None):
+    """
+    Run the simulation for the given PDB ID.
+
+    Args:
+        pdbid (str): The PDB ID.
+        input_pdb_path (str): The path to the input PDB file.
+        atomSubset (list or None): List of atom indices to subset. Defaults to None.
+
+    Returns:
+        None
+    """
+    
+    
+    print(f"Start simulation of {pdbid}...")
+    
+    # Input Files
+    pdb = PDBFile(input_pdb_path)
+    forcefield = ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
+
+    # System Configuration
+    nonbondedMethod = PME
+    nonbondedCutoff = 1.0*nanometers
+    ewaldErrorTolerance = 0.0005
+    constraints = HBonds
+    rigidWater = True
+    constraintTolerance = 0.000001
+    hydrogenMass = 1.5*amu
+
+    # Integration Options
+    dt = 0.002*picoseconds
+    temperature = 300*kelvin
+    friction = 1.0/picosecond
+    pressure = 1.0*atmospheres
+    barostatInterval = 25
+
+    # Simulation Options
+    steps = 1000
+    equilibrationSteps = 1000
+    platform = Platform.getPlatformByName('CUDA')
+    platformProperties = {'Precision': 'single'}
+
+    # Reporters
+    hdf5Reporter = HDF5Reporter(f'../data/{pdbid}/result/output_{pdbid}.h5', 100, atomSubset=atomSubset)
+    dataReporter = StateDataReporter(f'../data/{pdbid}/simulation/log.txt', 100, totalSteps=steps,
+        step=True, speed=True, progress=True, potentialEnergy=True, temperature=True, separator='\t')
+    checkpointReporter = CheckpointReporter(f'../data/{pdbid}/simulation/checkpoint.chk', 100)
+
+    # Prepare the Simulation
+    print('Building system...')
+    topology = pdb.topology
+    positions = pdb.positions
+    system = forcefield.createSystem(topology, nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff,
+        constraints=constraints, rigidWater=rigidWater, ewaldErrorTolerance=ewaldErrorTolerance, hydrogenMass=hydrogenMass)
+    system.addForce(MonteCarloBarostat(pressure, temperature, barostatInterval))
+    integrator = LangevinMiddleIntegrator(temperature, friction, dt)
+    integrator.setConstraintTolerance(constraintTolerance)
+    simulation = Simulation(topology, system, integrator, platform, platformProperties)
+    simulation.context.setPositions(positions)
+
+    # Write XML serialized objects
+    with open(f"../data/{pdbid}/simulation/system.xml", mode="w") as file:
+        file.write(XmlSerializer.serialize(system))
+    with open(f"../data/{pdbid}/simulation/integrator.xml", mode="w") as file:
+        file.write(XmlSerializer.serialize(integrator))
+
+    # Minimize and Equilibrate
+    print('Performing energy minimization...')
+    simulation.minimizeEnergy()
+    print('Equilibrating...')
+    simulation.context.setVelocitiesToTemperature(temperature)
+    simulation.step(equilibrationSteps)
+
+    # Simulate
+    print('Simulating...')
+    simulation.reporters.append(hdf5Reporter)
+    simulation.reporters.append(dataReporter)
+    simulation.reporters.append(checkpointReporter)
+    simulation.currentStep = 0
+
+    from sys import stdout
+    simulation.reporters.append(StateDataReporter(stdout, 100, step=True, 
+        progress=True, remainingTime=True, speed=True, totalSteps=steps, 
+        separator="\t"))
+
+
+    positions = []
+    forces = []
+    # Get postions and forces at each frame
+    for i in range(steps):
+        simulation.step(1)
+        # Create state object
+        state = simulation.context.getState(getPositions=True, getForces=True)
+        p = state.getPositions(asNumpy=True).value_in_unit(angstrom)
+        f = state.getForces(asNumpy=True).value_in_unit(kilojoules/mole/nanometer)
+        
+        # Save only the atoms of protein
+        if atomSubset is not None:
+            p = [p[i] for i in atomSubset]
+            f = [f[i] for i in atomSubset]
+        positions.append(p)
+        forces.append(f)
+    positions = np.array(positions)
+    forces = np.array(forces)
+    
+    # close hdf5 reporter
+    hdf5Reporter.close()
+
+    # Write file with final simulation state
+    simulation.saveState(f"../data/{pdbid}/simulation/final_state.xml")
+
+    state = simulation.context.getState(getPositions=True, enforcePeriodicBox=system.usesPeriodicBoundaryConditions())
+    with open(f"../data/{pdbid}/simulation/final_state.pdbx", mode="w") as file:
+        PDBxFile.writeFile(simulation.topology, state.getPositions(), file)
+
+    
+    # Write positions and forces to output h5 file
+    with h5py.File(f"../data/{pdbid}/result/output_{pdbid}.h5", "a") as f:
+        if "forces" not in f.keys():
+            f.create_dataset("forces", data=forces)
+        if "positions" not in f.keys():
+            f.create_dataset("positions", data=positions)
+
+    # Assert the data
+    with h5py.File(f"../data/{pdbid}/result/output_{pdbid}.h5", "a") as f:
+        # Check the shape of the data
+        assert f["positions"].shape == f["forces"].shape
+        # Check the dimension of the data
+        for key in ["positions", "forces"]:
+            assert f[key].shape[0] == steps
+            assert f[key].shape[1] == len(atomSubset)
+            assert f[key].shape[2] == 3
+            # Check if the data is not the same
+            for i in range(steps-1):
+                assert f[key][0,0,0] != f[key][i+1,0,0]
+    
+    print(f"Simulation of {pdbid} is done.")
+    print(f"Result is here: {f'../data/{pdbid}/result/output_{pdbid}.h5'}")
