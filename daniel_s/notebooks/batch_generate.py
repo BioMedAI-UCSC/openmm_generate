256a11454cac6b2d43097fe2f4dc7823877dbe3e
Daniel Sabo <DanielSabo@gmail.com>
2023-12-25 01:27:39 -0800

    batch_generate.py: Add --steps and --report-steps parameters, improve tracebacks

--- daniel_s/notebooks/batch_generate.py
+++ daniel_s/notebooks/batch_generate.py
@@ -3,6 +3,7 @@ sys.path.append("../../junya/openmm/scripts")
 
 import os
 import time
+import traceback
 from module import function
 from module import preprocess
 from module import simulation
@@ -55,7 +56,7 @@ def prepare_one(pdbid, data_dir=None):
         finished_file.write(finished_str)
     print(" ", finished_str)
 
-def simulate_one(pdbid, data_dir=None, steps=10000):
+def simulate_one(pdbid, data_dir=None, steps=10000, report_steps=1):
     if data_dir:
         function.set_data_dir(data_dir)
     finished_file_path = function.get_data_path(f'{pdbid}/simulation/finished.txt')
@@ -73,10 +74,10 @@ def simulate_one(pdbid, data_dir=None, steps=10000):
         try:
             pdb_path = function.get_data_path(f'{pdbid}/processed/{pdbid}_processed.pdb')
             atom_indices = function.get_non_water_atom_indexes(PDBFile(pdb_path).getTopology())
-            simulation.run(pdbid, pdb_path, steps, atomSubset=atom_indices)
+            simulation.run(pdbid, pdb_path, steps, report_steps=report_steps, atomSubset=atom_indices)
         except Exception as e:
             ok = False
-            print(e)
+            traceback.print_tb(e.__traceback__)
     with open(function.get_data_path(f'{pdbid}/simulation/{pdbid}_simulation.log'),"wb") as f:
         f.write(str(log).encode("utf-8"))
     t1 = time.time() - t0
@@ -102,7 +103,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch-index", required=True, type=int)
     parser.add_argument("--batch-size", required=True, type=int)
-    parser.add_argument("--pool-size", default=10, type=int)
+    parser.add_argument("--pool-size", default=10, type=int, help="Number of simultaneous simulations to run")
+    parser.add_argument("--steps", default=10000, type=int, help="Total number of steps to run")
+    parser.add_argument("--report-steps", default=1, type=int, help="Save data every n-frames")
     parser.add_argument("--data-dir", default="../data/", type=str)
     parser.add_argument("--gpus", default=None, type=str, help="A comma delimited lists of GPUs to use e.g. '0,1,2,3'")
 
@@ -136,7 +139,7 @@ def main():
     with multiprocessing.Pool(args.pool_size, initializer=init_function, initargs=init_args) as pool:
         pending_results = []
         for pdbid in batch_pdbid_list:
-            pending_results += [pool.apply_async(simulate_one, (pdbid, args.data_dir,))]
+            pending_results += [pool.apply_async(simulate_one, (pdbid, args.data_dir, args.steps, args.report_steps))]
         
         while pending_results:
             pending_results = [i for i in pending_results if not i.ready()]
