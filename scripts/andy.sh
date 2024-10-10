e725a9606907dcf5f338eb162113e879f27e453d
Andrew Chengong Bruce <acbruce@dt-login04.delta.ncsa.illinois.edu>
2024-10-10 16:32:22 -0500

    add comments for running on delta

--- scripts/andy.sh
+++ scripts/andy.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+#srun --account=bbpa-delta-gpu --partition=gpuA100x4 --gpus=4 --cpus-per-task=4 --mem=8g --time=12:00:00 ./andy.sh
+
 set -e
 
-IN_DIR="/media/DATA_18_TB_2/andy/tica_sampled_starting_poses/BBA_TYR_fixed/"
-OUT_DIR="/media/DATA_18_TB_1/andy/benchmark_set_5/BBA"
+IN_DIR="/u/acbruce/md_data/tica_sampled_starting_poses/chignolin_TYR_fixed"
+OUT_DIR="/u/acbruce/md_data/generate/chignolin"
 
 rm -fr "${OUT_DIR}"
 
 ./batch_generate.py \
     ./andy.json \
-    --gpus 1,2,3 \
+    --gpus 0,1,2,3 \
     --prepare \
     --integrator integrator_4fs.json \
     --remove-ligands \
     --steps 10000000 \
     --report-steps 100 \
+    --batch-size 60 \
+    --batch-index 0 \
     --input-dir "${IN_DIR}" \
     --data-dir "${OUT_DIR}"
